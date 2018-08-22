42ec0871104cf469e529101139baf53f5cb3bb38
elaubinger <elaubinger@yahoo.com>
2018-08-21 23:12:47 -0400

    Rough implementation of SoftFileInfo completed

--- IOSoftAccess/SoftAccess.cs
+++ IOSoftAccess/SoftAccess.cs
@@ -68,82 +68,127 @@ namespace IOSoftAccess
 
         public override bool TryGetFullName(out string fullName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                fullName = file.FullName;
+                return true;
+            }
+            catch (Exception)
+            {
+                fullName = default(string);
+                return false;
+            }
         }
 
         public override bool TryGetName(out string name)
         {
-            throw new NotImplementedException();
+            try
+            {
+                name = file.Name;
+                return true;
+            }
+            catch (Exception)
+            {
+                name = default(string);
+                return false;
+            }
         }
 
         public override bool TryGetAttributes(out FileAttributes attributes)
         {
-            throw new NotImplementedException();
+            try
+            {
+                attributes = file.Attributes;
+                return true;
+            }
+            catch (Exception)
+            {
+                attributes = default(FileAttributes);
+                return false;
+            }
         }
 
         public override bool TryGetCreationTime(out DateTime creationTime)
         {
-            throw new NotImplementedException();
+            try
+            {
+                creationTime = file.CreationTime;
+                return true;
+            }
+            catch (Exception)
+            {
+                creationTime = default(DateTime);
+                return false;
+            }
         }
 
         public override bool TryGetCreationTimeUtc(out DateTime creationTimeUtc)
         {
-            throw new NotImplementedException();
+            try
+            {
+                creationTimeUtc = file.CreationTimeUtc;
+                return true;
+            }
+            catch (Exception)
+            {
+                creationTimeUtc = default(DateTime);
+                return false;
+            }
         }
 
         public override bool TryGetLastAccessTime(out DateTime lastAccessTime)
         {
-            throw new NotImplementedException();
+            try
+            {
+                lastAccessTime = file.LastAccessTime;
+                return true;
+            }
+            catch (Exception)
+            {
+                lastAccessTime = default(DateTime);
+                return false;
+            }
         }
 
         public override bool TryGetLastAccessTimeUtc(out DateTime lastAccessTimeUtc)
         {
-            throw new NotImplementedException();
+            try
+            {
+                lastAccessTimeUtc = file.LastAccessTimeUtc;
+                return true;
+            }
+            catch (Exception)
+            {
+                lastAccessTimeUtc = default(DateTime);
+                return false;
+            }
         }
 
-        public override Task<bool> TryDeleteAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public override async Task<bool> TryDeleteAsync() 
+            => await Task.Run(() => TryDelete());
 
-        public override Task<(bool success, string extension)> TryGetExtensionAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public override async Task<(bool success, string extension)> TryGetExtensionAsync()
+            => await Task.Run(() => (TryGetExtension(out var extension), extension));
 
-        public override Task<(bool success, string fullName)> TryGetFullNameAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public override async Task<(bool success, string fullName)> TryGetFullNameAsync()
+            => await Task.Run(() => (TryGetFullName(out var fullName), fullName));
 
-        public override Task<(bool success, string name)> TryGetNameAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public override async Task<(bool success, string name)> TryGetNameAsync()
+            => await Task.Run(() => (TryGetName(out var name), name));
 
-        public override Task<(bool success, FileAttributes attributes)> TryGetAttributesAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public override async Task<(bool success, FileAttributes attributes)> TryGetAttributesAsync()
+            => await Task.Run(() => (TryGetAttributes(out var attributes), attributes));
 
-        public override Task<(bool success, DateTime creationTime)> TryGetCreationTimeAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public override async Task<(bool success, DateTime creationTime)> TryGetCreationTimeAsync()
+            => await Task.Run(() => (TryGetCreationTime(out var creationTime), creationTime));
 
-        public override Task<(bool success, DateTime creationTimeUtc)> TryGetCreationTimeUtcAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public override async Task<(bool success, DateTime creationTimeUtc)> TryGetCreationTimeUtcAsync()
+            => await Task.Run(() => (TryGetCreationTimeUtc(out var creationTimeUtc), creationTimeUtc));
 
-        public override Task<(bool success, DateTime lastAccessTime)> TryGetLastAccessTimeAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public override async Task<(bool success, DateTime lastAccessTime)> TryGetLastAccessTimeAsync()
+            => await Task.Run(() => (TryGetLastAccessTime(out var lastAccessTime), lastAccessTime));
 
-        public override Task<(bool success, DateTime lastAccessTimeUtc)> TryGetLastAccessTimeUtcAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public override async Task<(bool success, DateTime lastAccessTimeUtc)> TryGetLastAccessTimeUtcAsync()
+            => await Task.Run(() => (TryGetLastAccessTimeUtc(out var lastAccessTimeUtc), lastAccessTimeUtc));
     }
 }
